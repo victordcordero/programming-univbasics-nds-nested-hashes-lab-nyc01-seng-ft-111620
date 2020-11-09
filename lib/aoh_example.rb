5ad962ec26784f776784efb06aeac93b48b291a5
Victor Cordero <victordcordero@gmail.com>
2020-11-09 16:06:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ def assembled_aoh
   JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
   PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
   KATE_G = { name:  "Kate Gompert", occupation: "None" },
-  BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" },
+  BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 }
 end
 
